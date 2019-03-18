f0c4d8fdc8c4565dabf4ced697bd16c4ae836139
dhiva_risqyta_27rpl@student.smktelkom-mlg.sch.id <dhiva_risqyta_27rpl@student.smktelkom-mlg.sch.id>
2019-03-18 11:59:06 +0700

    first commit

---
+++ dosen/form-create.php
@@ -0,0 +1,12 @@
+<html>
+<h3>Tambah Data Dosen</h3>
+<body>
+<form method = "post" action="create.php">
+<table>
+<tr><td>Nama Dosen :</td><td><input type="text" name="nama_dosen"></td></tr>
+<tr><td>No.Telepon :</td><td><input type="text" name="telp"></td></tr>
+<tr><td colspan="2"><button type="submit" value="btnSimpan">SIMPAN</td></tr>
+</table>
+</form>
+</body>
+</html>
